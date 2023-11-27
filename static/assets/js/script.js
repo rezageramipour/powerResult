93636fd9d436a11ff456e3ca9c0fd3eea8845ff4
rezageramipour <rezageramipour7@gmail.com>
2023-11-27 09:48:40 -0800

    tamrin 5

---
+++ static/assets/js/script.js
@@ -0,0 +1,4 @@
+var userNumber1 = Number(prompt("عدد اول را وارد کنید:", 7));
+var userNumber2 = Number(prompt("عدد دوم را وارد کنید", 4));
+var powerResult = userNumber1 ** userNumber2;
+alert ("جواب عدد اول بر توان عدد دوم: " + powerResult);
